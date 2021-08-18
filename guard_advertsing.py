e41e0919d3e6abeac9c48735e86c04f9c0a9c9fd
xys20071111 <xys20071111@hotmail.com>
2021-08-18 22:10:20 +0800

    忘了while true了

--- guard_advertsing.py
+++ guard_advertsing.py
@@ -15,8 +15,8 @@ COLD_DOWN = config.get_advertising_cold_down()
 print_log('宣传语模块初始化完毕')
 
 async def guard_advertsing():
-    is_living:bool = (await LIVEROOM.get_room_info())['room_info']['live_status'] == 1
-    if is_living:
-        await send_danmaku(ADVERTISE,LIVEROOM)
-        print_log('发送宣传语')
-    await asyncio.sleep(COLD_DOWN)
+    while True:
+        if (await LIVEROOM.get_room_info())['room_info']['live_status'] == 1:
+            await send_danmaku(ADVERTISE,LIVEROOM)
+            print_log('发送宣传语')
+        await asyncio.sleep(COLD_DOWN)
